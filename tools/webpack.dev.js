eeb1603dd5946974140134259013086833c9a05f
Jain, Sneha T <Sneha.T.Jain@in.tesco.com>
2017-05-20 08:11:26 +0530

    web pack upgraded

--- tools/webpack.dev.js
+++ tools/webpack.dev.js
@@ -1,37 +1,68 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import webpack from 'webpack';
 import path from 'path';
 
-export default {
-  debug: true,
+module.exports = {
   devtool: '#inline-source-map',
-  noInfo: false,
-  colors: true,
   entry: [
     'webpack-hot-middleware/client?reload=true',
     './tools/main.js'
   ],
-  target: 'web',
   output: {
     path: path.join(__dirname, '../public'),
     publicPath: '/',
     filename: 'main.js'
   },
   devServer: {
-    contentBase: './src'
+    contentBase: './src',
+    stats: 'errors-only'
+  },
+  node: {
+    module: 'empty',
+    fs: 'empty'
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      use: [{ loader: 'babel-loader' }]
+    }, {
+      test: /\.css$/,
+      use: [{ loader: 'style-loader' },
+        { loader: 'css-loader' }
+      ]
+    }, {
+      test: /\.less$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader'
+      }, {
+        loader: 'less-loader'
+      }]
+    }, {
+      test: /\.(eot|svg|ttf|woff|woff2)$/,
+      use: [{ loader: 'file-loader', options: { name: 'fonts/[name]-[hash].[ext]' } }]
+    }]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    // new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        debug: true,
+        noInfo: true,
+        colors: true,
+      }
+    })
   ],
-  module: {
-    loaders: [
-      {test: /\.jsx?$/, loaders: ['babel']},
-      {test: /(\.css)$/, loaders: ['style', 'css']},
-      {test: /\.less$/, loader: 'style!css!less'},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
-    ]
+  stats: {
+    children: false // prevent outputs from child plugins like from extract-text-plugin
+  },
+  /* will always resolve from local and prevent addition of duplicate dependencies */
+  resolve: {
+    alias: {
+      react: path.resolve('./node_modules/react')
+    }
   }
 };
