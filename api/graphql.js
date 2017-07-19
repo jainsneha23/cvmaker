45b3da6a7dc032a5c545c6085ae55ec8f433ec6e
Jain, Sneha T <Sneha.T.Jain@in.tesco.com>
2017-07-19 08:38:22 +0530

    fronted integrated with graphql

--- api/graphql.js
+++ api/graphql.js
@@ -1,122 +1,165 @@
 import {
   GraphQLObjectType,
-  GraphQLID,
   GraphQLString,
-  GraphQLBoolean,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLFloat,
   GraphQLList,
   GraphQLNonNull,
   GraphQLSchema
 } from 'graphql';
 
-const dbName = 'TODOS';
-const db = global.db;
+const dbName = 'RESUMES';
 
-const TodoType = new GraphQLObjectType({  
-  name: 'todo',
-  fields: function () {
-    return {
-      id: {
-        type: GraphQLID
-      },
-      title: {
-        type: GraphQLString
-      },
-      completed: {
-        type: GraphQLBoolean
+class ResumeSchema {
+
+  constructor() {
+    const db = global.db;
+
+    db.createTable(dbName).catch(() => {
+      throw `Exception in creating DB: ${dbName}`;
+    });
+
+    this.createScema();
+  }
+
+  createScema() {
+    const db = global.db;
+
+    const ResumeType = new GraphQLObjectType({  
+      name: 'resume',
+      fields: function () {
+        return {
+          id: {
+            type: GraphQLID
+          },
+          userid: {
+            type: GraphQLFloat
+          },
+          resumeid: {
+            type: GraphQLInt
+          },
+          cvdata: {
+            type: GraphQLString
+          }
+        };
       }
-    };
-  }
-});
-
-const MutationAdd = {
-  type: new GraphQLList(TodoType),
-  description: 'Add a Todo',
-  args: {
-    title: {
-      name: 'Todo title',
-      type: new GraphQLNonNull(GraphQLString)
-    }
-  },
-  resolve: (root, {title}) => {
-    return new Promise((resolve, reject) => {
-      db.insert(dbName, {title})
-      .then(() => db.selectAll(dbName))
-      .then(todos => resolve(todos))
-      .catch(err => reject(err));
     });
-  }
-};
 
-const MutationDelete = {
-  type: new GraphQLList(TodoType),
-  description: 'Delete a Todo',
-  args: {
-    id: {
-      name: 'Todo Id',
-      type: new GraphQLNonNull(GraphQLID)
-    }
-  },
-  resolve: (root, {id}) => {
-    return new Promise((resolve, reject) => {
-      db.delete(dbName, {id})
-      .then(() => db.selectAll(dbName))
-      .then(todos => resolve(todos))
-      .catch(err => reject(err));
-    });
-  }
-};
-
-const MutationUpdate = {
-  type: new GraphQLList(TodoType),
-  description: 'Update a Todo',
-  args: {
-    id: {
-      name: 'Todo Id',
-      type: new GraphQLNonNull(GraphQLID)
-    },
-    title: {
-      name: 'Todo title',
-      type: new GraphQLNonNull(GraphQLString)
-    }
-  },
-  resolve: (root, {id, title}) => {
-    return new Promise((resolve, reject) => {
-      db.delete(dbName, {id, title})
-      .then(() => db.selectAll(dbName))
-      .then(todos => resolve(todos))
-      .catch(err => reject(err));
-    });
-  }
-};
-
-const queryType = new GraphQLObjectType({  
-  name: 'Query',
-  fields: () => {
-    return {
-      todos: {
-        type: new GraphQLList(TodoType),
-        resolve: () => {
-          return new Promise((resolve, reject) => {
-            db.selectAll(dbName)
-            .then(todos => resolve(todos))
-            .catch(err => reject(err));
-          });
+    const MutationAdd = {
+      type: new GraphQLList(ResumeType),
+      description: 'Add a Resume',
+      args: {
+        id: {
+          name: 'id',
+          type: new GraphQLNonNull(GraphQLID)
+        },
+        userid: {
+          name: 'User ID',
+          type: new GraphQLNonNull(GraphQLFloat)
+        },
+        resumeid: {
+          name: 'Resume id',
+          type: new GraphQLNonNull(GraphQLInt)
+        },
+        cvdata: {
+          name: 'Resume data',
+          type: new GraphQLNonNull(GraphQLString)
         }
+      },
+      resolve: (root, {id, userid, resumeid, cvdata}) => {
+        return new Promise((resolve, reject) => {
+          db.insert(dbName, {id, userid, resumeid, cvdata})
+          .then(() => db.selectAll(dbName))
+          .then(resumes => resolve(resumes))
+          .catch(err => reject(err));
+        });
       }
     };
-  }
-});
 
-const MutationType = new GraphQLObjectType({
-  name: 'Mutation',
-  fields: {
-    add: MutationAdd,
-    delete: MutationDelete,
-    update: MutationUpdate
-  }
-});
+    const MutationDelete = {
+      type: new GraphQLList(ResumeType),
+      description: 'Delete a Resume',
+      args: {
+        id: {
+          name: 'Id',
+          type: new GraphQLNonNull(GraphQLID)
+        }
+      },
+      resolve: (root, {id}) => {
+        return new Promise((resolve, reject) => {
+          db.delete(dbName, {id})
+          .then(() => db.selectAll(dbName))
+          .then(resumes => resolve(resumes))
+          .catch(err => reject(err));
+        });
+      }
+    };
 
-export default new GraphQLSchema({  
-  query: queryType,
-  mutation: MutationType
-});
+    const MutationUpdate = {
+      type: new GraphQLList(ResumeType),
+      description: 'Update a Resume',
+      args: {
+        id: {
+          name: 'Id',
+          type: new GraphQLNonNull(GraphQLID)
+        },
+        cvdata: {
+          name: 'Resume data',
+          type: new GraphQLNonNull(GraphQLString)
+        }
+      },
+      resolve: (root, {id, cvdata}) => {
+        return new Promise((resolve, reject) => {
+          db.update(dbName, {id}, {$set:{cvdata}})
+          .then(() => db.selectAll(dbName))
+          .then(resumes => resolve(resumes))
+          .catch(err => reject(err));
+        });
+      }
+    };
+
+    this.queryType = new GraphQLObjectType({  
+      name: 'Query',
+      fields: () => {
+        return {
+          resumes: {
+            type: new GraphQLList(ResumeType),
+            description: 'Get a Resume',
+            args: {
+              userid: {
+                name: 'User ID',
+                type: new GraphQLNonNull(GraphQLFloat)
+              }
+            },
+            resolve: (root, {userid}) => {
+              return new Promise((resolve, reject) => {
+                db.find(dbName, {userid})
+                .then(resumes => resolve(resumes))
+                .catch(err => reject(err));
+              });
+            }
+          }
+        };
+      }
+    });
+
+    this.MutationType = new GraphQLObjectType({
+      name: 'Mutation',
+      fields: {
+        add: MutationAdd,
+        delete: MutationDelete,
+        update: MutationUpdate
+      }
+    });
+  }
+
+  getSchema() {
+    return new GraphQLSchema({  
+      query: this.queryType,
+      mutation: this.MutationType
+    });
+  }
+}
+
+export default ResumeSchema;
